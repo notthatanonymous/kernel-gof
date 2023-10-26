5ca7f9de047c8052f91cf62021f33d12fe22d49b
Ahmad Abdellatif <124534399+notthatanonymous@users.noreply.github.com>
2023-10-26 17:18:18 -0400

    Create example.py

---
+++ example.py
@@ -0,0 +1,12 @@
+import kgof
+import kgof.data as data
+import kgof.density as density
+import kgof.goftest as gof
+import kgof.kernel as kernel
+import kgof.plot as plot
+import kgof.util as util
+
+import matplotlib
+#import matplotlib.pyplot as plt
+import autograd.numpy as np
+# import scipy.stats as stats
